fc510f069bec157a5544592ab6211ed21fab77bb
Yoshiyuki <yosshit47@gmail.com>
2023-05-01 22:58:38 +0900

    Added type to props in input field component

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function InputField (props) {
+function InputField (props: JSX.IntrinsicAttributes & React.ClassAttributes<HTMLInputElement> & React.InputHTMLAttributes<HTMLInputElement>) {
     const className = `px-3 py-1 rounded-md border border-slate-200 focus:border-indigo-400 bg-slate-100 focus:bg-white focus:border-1 focus:outline-none st ${props.className || ''}`
     return (
         <div className='flex flex-col'>
