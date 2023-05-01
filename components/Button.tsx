292f53269eef16cc0139445745fac5b7824694b7
Yoshiyuki <yosshit47@gmail.com>
2023-05-01 22:53:53 +0900

    Fixed props type in Button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Button (props) {
+function Button (props: JSX.IntrinsicAttributes & React.ClassAttributes<HTMLButtonElement> & React.ButtonHTMLAttributes<HTMLButtonElement>) {
     const className = `flex items-center justify-center px-3 py-1 rounded-md text-white bg-indigo-600 hover:bg-indigo-700 disabled:bg-indigo-400 ${props.className || ''}`
     return (
         <div>
